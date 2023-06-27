e9b1f40db9b6a8e69fc6e8a713a5d56716413dbd
Hanisafa <mhanisafa@hotmail.com>
2023-06-27 12:38:10 +0300

    sdfsdfsdf

--- GitTests/F1.cs
+++ GitTests/F1.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GitTests
+namespace GitTests;
+
+internal class F1
 {
-    internal class F1
-    {
-        public int MyProperty { get; set; }
+    public int MyProperty { get; set; }
 
-        public int MyProperty1 { get; set; }
+    public int MyProperty1 { get; set; }
 
-        public int MyProperty2 { get; set; }
+    public int MyProperty2 { get; set; }
 
-        public int MyProperty3 { get; set;}
-        public int MyProperty4 { get; set; }
-    }
+    public int MyProperty3 { get; set;}
+    public int MyProperty4 { get; set; }
+
+    public int MyProperty5 { get; set; }
+}
 
 
 public class FF1
